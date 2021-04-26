8895b19a033d3164a61cfa2155362c852070d67c
vimaleshn98 <vimaleshn98@gmail.com>
2021-04-26 14:09:38 +0000

    node attributr

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -4,11 +4,24 @@ end
 package 'ntp' do
  action :install
 end
+ip = node['ipaddress']
+host =node['hostname']
+# print apple count
+apple = 4
+puts " i have #{apple } apples"
+
+
+
 package 'git' do
  action :install
 end
 file '/etc/motd' do
- content 'Property of azure user'
+ content "Property of azure user 
+      IP-address: #{ip}
+      Disk-size: #{node['memory']['total']}
+	HostName: #{host}
+	CPU: #{node['cpu']['0']['mhz']}
+     "
  owner 'root'
  group 'root'
 end
--- workstation/recipes/webserver.rb
+++ workstation/recipes/webserver.rb
@@ -1,10 +1,11 @@
 package 'httpd' do
   action :install
 end
-
+ip = node['ipaddress']
+host =node['hostname']
 
 file 'var/www/html/index.html' do
-  content '<h1> Hello Chef </h1>'
+  content "<h1> Hello Chef with #{ip} and host: #{host} </h1>"
 end
 
 service 'httpd' do
